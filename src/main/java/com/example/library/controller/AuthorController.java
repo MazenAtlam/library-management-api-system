7176b9c67f1976d64b3559251bf8bed3feb825cf
mazen-atlam <mazenatef5510@gmail.com>
2026-04-21 01:51:01 +0200

    Resolve unused imports

--- src/main/java/com/example/library/controller/AuthorController.java
+++ src/main/java/com/example/library/controller/AuthorController.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/authors")
 public class AuthorController {
--- src/main/java/com/example/library/service/AuthorService.java
+++ src/main/java/com/example/library/service/AuthorService.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 
